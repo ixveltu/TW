7b17a785115fef4f3729becb8634bd80f853528f
Emanuel Gomes <emanuel.jose.f.gomes@gmail.com>
2025-01-29 12:47:19 +0000

    Delete trabalhoTW directory

--- trabalhoTW/scripts/inquiry.js
+++
@@ -1,6 +0,0 @@
-function load_data() {
-  urlParams = new URLSearchParams(window.location.search);
-  courseTitle = urlParams.get("course");
-  courseTitle = courseTitle.replaceAll("_", " ");
-  document.getElementById("title").innerHTML = courseTitle;
-}
